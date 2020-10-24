d901ea54b670f00435ffa98737b95373d80bd5b1
Hulda <huldas96@gmail.com>
2020-10-24 15:26:52 +0000

    fleiri js files

---
+++ JS/paint.class.js
@@ -0,0 +1,12 @@
+export default class Paint {
+
+    constructor(canvasId) {
+
+        this.canvas = document.getElementById(canvasId);
+        this.context = canvas.getContext("2d");
+    }
+
+    set activeTool(tool) {
+        this.tool = tool;
+    }
+}
