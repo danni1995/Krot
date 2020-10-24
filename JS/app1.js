179e191864ca9d58bfa42a72618e7ba273d239c2
Hulda <huldas96@gmail.com>
2020-10-24 15:28:19 +0000

    yes

--- JS/app1.js
+++ JS/app1.js
@@ -1,4 +1,7 @@
-import {TOOL_BUCKET, TOOL_COLORWHEEL, TOOL_DOTS, TOOL_ERASER, TOOL_EYEDROP, TOOL_PEN, TOOL_SHAPES, TOOL_SHAPES_CIRCLE, TOOL_SHAPES_RECTANGLE, TOOL_SHAPES_TRIANGLE, TOOL_TEXT} from './tool.class.js';
+import {TOOL_BUCKET, TOOL_COLORWHEEL, TOOL_DOTS, TOOL_ERASER, TOOL_EYEDROP, TOOL_PEN, TOOL_SHAPES, TOOL_SHAPES_CIRCLE, TOOL_SHAPES_RECTANGLE, TOOL_SHAPES_TRIANGLE, TOOL_TEXT} from './tool.js';
+import Paint from './paint.class.js';
+
+var paint = new Paint("my-canvas");
 
 
 /* Here we select all the tools by finding them in the html with "[data-tool]".
@@ -10,9 +13,17 @@ document.querySelectorAll("[data-tool]").forEach(
             document.querySelector("[data-tool].clicked").classList.toggle("clicked"); // remove the class "clicked" from last used tool
             item.classList.toggle("clicked"); // Adds the class "clicked"
 
-            let selectedTool = item.getAttribute("data-tool");
+            let = selectedTool = item.getAttribute("data-tool");
 
+            switch (selectedTool) { // if the tool that is selected is tool_shapes then the triangle, circle and rectangle tools will pop up.
+                case tool.TOOL_SHAPES:
+                    document.querySelector(".shapes-wrapper").style.display = "block";
+                    break;
+            default:
+                document.querySelector(".shapes-wrapper").style.display = "none";
+            }
 
         });
     }
 );
+
