41b9fb77a232a2d1b09002c91c7edd42e9eec799
Hulda <huldas96@gmail.com>
2020-10-24 16:37:25 +0000

    laga

--- JS/app1.js
+++ JS/app1.js
@@ -4,6 +4,8 @@ import {TOOL_BUCKET, TOOL_COLORWHEEL, TOOL_DOTS, TOOL_ERASER, TOOL_EYEDROP, TOOL
 
 
 let paint = new Paint('my-canvas');
+paint.activeTool = TOOL_PEN;
+paint.init();
 
 var canvas = document.getElementById("my-canvas");
 var ctx = canvas.getContext("2d");
--- JS/paint.class.js
+++ JS/paint.class.js
@@ -4,4 +4,25 @@ export default class Paint {
         this.tool = tool;
         console.log(this.tool);
     }
+
+    init() {
+        this.canvas.onmousedown = e => this.onMouseDown(e);
+    }
+
+    onMouseDown(e) {
+
+        this.canvas.onmousedown = e => this.onMouseDown(e);
+        document.onmouseup = e => this.onMouseUp(e);
+
+        console.log(e.clientX, e.clientY);
+    }
+
+    onMouseMove(e) {
+        console.log(e.clientX, e.clientY);
+    }
+
+    onMouseUp(e) {
+        this.canvas.onmousedown = null;
+        document.onmouseup = null;
+    }
 }
