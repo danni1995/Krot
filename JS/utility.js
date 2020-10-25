e429d2abb6d645f77f9552098ff2a3ab20f2206c
Hulda <huldas96@gmail.com>
2020-10-25 16:39:17 +0000

    update

--- JS/utility.js
+++ JS/utility.js
@@ -7,7 +7,7 @@ export function getMouseLocationOnCanvas(e, canvas) { // Fuction that gets the l
     return new Point(x ,y ); // this function will return a detailed mouse position by going trough the constructor in point.model.js. Why? So we can use this function again and again.
 }
 
-// This function calculates the distance between where your mouse is located when down and where it ends when mouse up. (for the circle)
+// This function calculates the distance between where you start holding your mouse and where it ends when mouse is released. (for the circle)
 export function findDistance(location1, location2) { // Here is a photo to explain what we are doing: https://imgur.com/a/ADGfZYd
 
     let exp1 = Math.pow(location2.x - location1.x, 2);
